89ac93c520081433d5ae7368978c0b009cc5497e
akrah <akrah>
2016-03-02 15:41:36 +0100

    Minor cleaning

--- visualisation/graphViewer.cpp
+++ visualisation/graphViewer.cpp
@@ -31,11 +31,8 @@
 #include <DGtal/io/viewers/Viewer3D.h>
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
-#include <DGtal/geometry/surfaces/COBANaivePlaneComputer.h>
 #include <DGtal/io/readers/PointListReader.h>
 #include <DGtal/io/readers/TableReader.h>
-
-// #include <DGtal/io/writers/MeshWriter.h>
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
@@ -46,40 +43,43 @@ using namespace DGtal;
 namespace po = boost::program_options;
 
 int main( int argc, char** argv )
-{
- 
+{ 
+  QApplication application(argc,argv);
+
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
-  ("help,h", "display this message")
-  ("inputEdge,e", po::value<std::string>(), "input file containing the edge list.")
-  ("inputVertex,v", po::value<std::string>(), "input file containing the vertex list.")
-  ("inputRadii,r", po::value<std::string>(), "input file containing the radius for each edge.")
-  ("ballRadius,b", po::value<double>()->default_value(2.0), "radius of edge balls.");
+    ("help,h", "display this message")
+    ("inputEdge,e", po::value<std::string>(), "input file containing the edge list.")
+    ("inputVertex,v", po::value<std::string>(), "input file containing the vertex list.")
+    ("inputRadii,r", po::value<std::string>(), "input file containing the radius for each edge.")
+    ("ballRadius,b", po::value<double>()->default_value(2.0), "radius of edge balls.");
 
   bool parseOK = true;
 
   // Process options in arguments
-  QApplication application(argc,argv);
   po::variables_map vm;
-  try{
+  try
+  {
     po::store(po::parse_command_line(argc, argv, general_opt), vm);
-  }catch(const std::exception& ex){
-    trace.info()<< "Error checking program options: "<< ex.what()<< std::endl;
-    parseOK=false;
+  }
+  catch( const std::exception& ex )
+  {
+    trace.info() << "Error checking program options: " << ex.what() << std::endl;
+    parseOK = false;
   }
   po::notify(vm);
   if( !parseOK || argc<=1 || vm.count("help") || !vm.count("inputEdge")  || !vm.count("inputVertex") )
   {
-    trace.info()<< "Basic display graph" <<std::endl << "Options: "<<std::endl
-		  << general_opt << "\n";
+    trace.info() << "Basic display graph" << std::endl
+                 << "Options:" << std::endl
+		             << general_opt << std::endl;
     return 1;
   }
-
-  bool useRadiiFile = vm.count("inputRadii");
 
   std::string nameFileEdge = vm["inputEdge"].as<std::string>();
   std::string nameFileVertex = vm["inputVertex"].as<std::string>();
   double r = vm["ballRadius"].as<double>();
+  bool useRadiiFile = vm.count("inputRadii");
 
   // Structurex to store edges and vertex read in input files
   std::vector<Z3i::RealPoint> vectEdges = PointListReader<Z3i::RealPoint>::getPointsFromFile(nameFileEdge);
@@ -101,31 +101,26 @@ int main( int argc, char** argv )
 
   typedef Viewer3D<> MyViewer;
   MyViewer viewer;
+  viewer << CustomColors3D( Color::Blue, Color::Blue );
 
   // Add edges to viewer as balls
-  viewer << CustomColors3D( Color::Blue, Color::Blue );
   for ( int i=0 ; i<vectEdges.size() ; ++i )
   {
     viewer.addBall(vectEdges[i], vectRadii[i]);
   }
 
   // Add vertex to viewer as lines
-  viewer << CustomColors3D( Color::Blue,  Color::Blue );
   Mesh<Z3i::RealPoint> aMesh;
+  std::vector<Z3i::RealPoint> vertex;
+  std::vector<double> radii;
   for ( const auto& v: vectVertex )
   {
-    const Z3i::RealPoint& e1 = vectEdges[v[0]];
-    const Z3i::RealPoint& e2 = vectEdges[v[1]];
-    viewer.addLine(e1, e2, 10);
-    std::vector<Z3i::RealPoint> vertex = { e1, e2 };
-    std::vector<double> radii = { vectRadii[v[0]], vectRadii[v[1]] };
-    Mesh<Z3i::RealPoint>::createTubularMesh(aMesh, vertex, radii, 0.1, DGtal::Color::Green);
+    vertex = { vectEdges[v[0]], vectEdges[v[1]] };
+    radii = { vectRadii[v[0]], vectRadii[v[1]] };
+    Mesh<Z3i::RealPoint>::createTubularMesh(aMesh, vertex, radii, 0.1);
   }
 
   viewer << aMesh;
-
-  // aMesh >> "toto.off";
-  
   viewer << MyViewer::updateDisplay;
   viewer.show();
 
