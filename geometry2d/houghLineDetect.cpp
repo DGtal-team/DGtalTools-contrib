c16b634f29696b76e1a05e9223ec80778366abd5
Kerautret <bertrand.kerautret@univ-lorraine.fr>
2016-08-29 11:07:13 +0200

    readme

--- geometry2d/houghLineDetect.cpp
+++ geometry2d/houghLineDetect.cpp
@@ -1,3 +1,34 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file houghLineDetect.cpp
+ * @ingroup geometry2d
+ * @author Bertrand Kerautret (\c kerautre@loria.fr )
+ * LORIA (CNRS, UMR 7503), University of Lorraine, France
+ *
+ * @date 2016/08/29
+ *
+ * Source file of the tool houghLineDetect
+ *
+ * This file is part of the DGtal library/DGtalTools-contrib Project.
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include "DGtal/base/Common.h"
 
 #include <fstream>
 #include "opencv2/highgui/highgui.hpp"
@@ -10,18 +41,51 @@
 #include <iostream>
 
 using namespace cv;
-using namespace std;
 namespace po = boost::program_options;
 
 
+using namespace std;
+using namespace DGtal;
+///////////////////////////////////////////////////////////////////////////////
+namespace po = boost::program_options;
 
 
-int main(int argc, char** argv)
+/**
+ @page houghLineDetect houghLineDetect
+ 
+ @brief  Apply the hough transform from the OpenCV implementation.
+
+ @b Usage:   houghLineDetect [input]
+
+ @b Allowed @b options @b are :
+ 
+ @code
+  -h [ --help ]           display this message
+  -i [ --input ] arg      an input file... 
+  -p [ --parameter] arg   a double parameter...
+ @endcode
+
+ @b Example: 
+
+ @code
+   	houghLineDetect -i  $DGtal/examples/samples/....
+ @endcode
+
+ @image html reshoughLineDetect.png "Example of result. "
+
+ @see
+ @ref houghLineDetect.cpp
+
+ */
+
+
+int main( int argc, char** argv )
 {
-  po::options_description general_opt("Specific allowed options (for Qt options, see Qt official site) are");
+  // parse command line -------------------------------------------------------
+  po::options_description general_opt("Allowed options are");
   general_opt.add_options()
     ("help,h", "display this message")
-    ("input,i", po::value<std::string>(), "the input image file." )
+     ("input,i", po::value<std::string>(), "the input image file." )
     ("output,o", po::value<std::string>(), "the output file containing the resulting lines segments (one segment per lines)." )
     ("rho,r", po::value<double>()->default_value(1.0), "The resolution of the parameter r in pixels. We use 1 pixel." )
     ("theta,t", po::value<double>()->default_value(CV_PI/180.0), "The resolution of the parameter \theta in radians." )
@@ -31,28 +95,37 @@ int main(int argc, char** argv)
     ("useProbabilist,P","use improved probabilist algorithm" );
   
   bool parseOK=true;
-
   po::variables_map vm;
-  try{
-    po::store(po::parse_command_line(argc, argv, general_opt), vm);
-  }catch(const std::exception& ex){
-    parseOK=false;
-    cout<< "Error checking program options: "<< ex.what()<< endl;
-  }
+  try
+    {
+      po::store(po::parse_command_line(argc, argv, general_opt), vm);
+    }
+  catch(const std::exception& ex)
+    {
+      parseOK=false;
+      DGtal::trace.info()<< "Error checking program options: "<< ex.what()<< endl;
+    }
+  
+
+  // check if min arguments are given and tools description ------------------
   po::notify(vm);
   if( !parseOK || vm.count("help")||argc<=1)
     {
       std::cout << "Usage: " << argv[0] << " [input]\n"
-                << "Apply OpenCV Hough Transform for line finding."
-                << general_opt << "\n";
+                << "Apply the Hough transform from the OpenCV implementation.\n"
+                << general_opt << "\n"
+                << "Typical use example:\n \t houghLineDetect -i ... \n";
       return 0;
+    }  
+  if(! vm.count("input"))
+    {
+      DGtal::trace.error() << " The file name was not defined" << endl;
+      return 1;
     }
+
   const char* filename;
-  if(vm.count("input")){
-    filename = vm["input"].as<std::string>().c_str();
-  }
-
-
+  filename = vm["input"].as<std::string>().c_str();
+  
   Mat src = imread(filename, 0);
   if(src.empty())
     {
@@ -61,8 +134,7 @@ int main(int argc, char** argv)
     }
 
 
-
-  // get  hough params:
+  //  recover the  args ----------------------------------------------------
   double rho = vm["rho"].as<double>();
   double theta = vm["theta"].as<double>();
   double threshold = vm["threshold"].as<double>();
@@ -112,16 +184,14 @@ int main(int argc, char** argv)
           line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 1, CV_AA);
           if(outLines){
             outStream << l[0] << " " << l[1] << " " << l[2] << " " << l[3] << std::endl;
-          }
-        
+          }        
         }
     }
-    
+  
   imshow("source", src);
   imshow("detected lines", cdst);
-
   waitKey();
-
+  
   return 0;
 }
 
