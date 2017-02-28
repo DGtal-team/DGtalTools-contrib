fbe5764df02c3f5088e94656dd4085319be234be
Nicolas Normand <Nicolas.J.Normand+github@gmail.com>
2017-02-28 12:31:56 +0100

    Changed affiliation

--- geometry2d/distanceTransform/ImageWriter.cpp
+++ geometry2d/distanceTransform/ImageWriter.cpp
@@ -17,7 +17,8 @@
  * @file ImageWriter.cpp
  * @ingroup Tools
  * @author Nicolas Normand (\c Nicolas.Normand@polytech.univ-nantes.fr)
- * LUNAM Université, Université de Nantes, IRCCyN UMR CNRS 6597
+ * Université Bretagne Loire, Université de Nantes,
+ * Laboratoire des Sciences du Numérique de Nantes (LS2N) UMR CNRS 6004
  *
  * @date 2012/09/28
  *
