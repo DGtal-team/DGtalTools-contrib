280592f6f3bbb29f0c231902d1d071f04ed6ac4e
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2018-12-26 19:50:35 +0100

    david remarks

--- visualisation/displayLineSegments.cpp
+++ visualisation/displayLineSegments.cpp
@@ -49,11 +49,7 @@
 #include "DGtal/io/readers/PointListReader.h"
 #include "DGtal/io/Color.h"
 
- #include "DGtal/io/readers/GenericReader.h"
-
-
-
-
+#include "DGtal/io/readers/GenericReader.h"
 
 //contour
 #include "DGtal/geometry/curves/FreemanChain.h"
@@ -96,24 +92,24 @@ namespace po = boost::program_options;
 
   -h [ --help ]           display this message
   -i [ --input ] arg      the input file containing the segments x1 y1 x2 y2 to be displayed.
-  -s [ --inputSecSet ] arg the second input set file containing the segments x1 y1 x2 y2 to be displayed.
+  -s [ --inputSec ]       arg the second input set file containing the segments x1 y1 x2 y2 to be displayed.
   --SDPindex arg          specify the sdp index of segment endpoints (by
                           default 0,1,2,3).
   --lineWidth arg (=1)    Define the linewidth of the segments.
-  --lineWidthSecSet arg (=1) Define the linewidth of the segments (second Set).
+  --lineWidthSec arg (=1) Define the linewidth of the segments (second Set).
   --noXFIGHeader           to exclude xfig header in the resulting output
                             stream (no effect with option -outputFile).
  --customLineColor arg   set the R, G, B, A components of the colors of
                           the mesh faces and eventually the color R, G, B,
                           A of the mesh edge lines (set by default to black).
-  --customLineColorSecSet arg  set the R, G, B, A components of the colors of
+  --customLineColorSec arg  set the R, G, B, A components of the colors of
                           the mesh faces and eventually the color R, G, B,
                           A of the mesh edge lines (set by default to black).
   --customPointColor arg  set the R, G, B, A components of the colors of
                            the mesh faces and eventually the color R, G, B,
                            A of the mesh edge lines (set by default to black).
-  -e [ --noDisplayEndPoints ]  to not displays segment end points.
-  --noDisplayEndPointsSecSet   to not displays segment end points of the second
+  -e [ --noDisplayEndPoints ]  to not display segment end points.
+  --noDisplayEndPointsSec   to not display segment end points of the second
                           set.
   -o [ --outputFile ] arg  <filename> save output file automatically according
                           the file format extension.
@@ -150,59 +146,65 @@ Typical use example:
 
 void displayLineSet(po::variables_map vm, std::string nameInput,
                     std::string nameArgCol, Board2D &aBoard,
-                    unsigned int height, bool invertYaxis, bool displayEndPoint, int lineWidth=2)
+                    unsigned int height, bool invertYaxis,
+                    bool displayEndPoint, int lineWidth=2)
 {
-    std::string fileName = vm[nameInput].as<std::string>();
-    std::vector<unsigned int > vectPos;
+  std::string fileName = vm[nameInput].as<std::string>();
+  std::vector<unsigned int > vectPos;
     
-    if(vm.count("SDPindex"))
+  if(vm.count("SDPindex"))
+  {
+    vectPos = vm["SDPindex"].as<std::vector<unsigned int > >();
+    if(vectPos.size()!=4)
     {
-        vectPos = vm["SDPindex"].as<std::vector<unsigned int > >();
-        if(vectPos.size()!=4)
-        {
-            trace.error() << "you need to specify the four indexes of vertex." << std::endl;
-            return ;
-        }
-    }else
+      trace.error() << "you need to specify the four indexes of vertex." << std::endl;
+      return ;
+    }
+  }else
+  {
+    vectPos = {0,1,2,3};
+  }
+  std::vector<unsigned int> vectPos1; vectPos1.push_back(vectPos[0]); vectPos1.push_back(vectPos[1]);
+  std::vector<Z2i::RealPoint> vectPt1 = PointListReader<Z2i::RealPoint>::getPointsFromFile(fileName, vectPos);
+  std::vector<unsigned int> vectPos2; vectPos2.push_back(vectPos[2]); vectPos2.push_back(vectPos[3]);
+  std::vector<Z2i::RealPoint> vectPt2 = PointListReader<Z2i::RealPoint>::getPointsFromFile(fileName, vectPos2);
+    
+  DGtal::Color lineColor = DGtal::Color::Red;
+  DGtal::Color pointColor = DGtal::Color::Blue;
+  if(vm.count(nameArgCol))
+  {
+    std::vector<unsigned int > vectCol = vm[nameArgCol].as<std::vector<unsigned int> >();
+    if(vectCol.size()!=3 )
     {
-        vectPos = {0,1,2,3};
+      trace.error() << "colors specification should contain R,G,B values (using default red)."<< std::endl;
     }
-    std::vector<unsigned int> vectPos1; vectPos1.push_back(vectPos[0]); vectPos1.push_back(vectPos[1]);
-    std::vector<Z2i::RealPoint> vectPt1 = PointListReader<Z2i::RealPoint>::getPointsFromFile(fileName, vectPos);
-    std::vector<unsigned int> vectPos2; vectPos2.push_back(vectPos[2]); vectPos2.push_back(vectPos[3]);
-    std::vector<Z2i::RealPoint> vectPt2 = PointListReader<Z2i::RealPoint>::getPointsFromFile(fileName, vectPos2);
-    
-    DGtal::Color lineColor = DGtal::Color::Red;
-    DGtal::Color pointColor = DGtal::Color::Blue;
-    if(vm.count(nameArgCol)){
-        std::vector<unsigned int > vectCol = vm[nameArgCol].as<std::vector<unsigned int> >();
-        if(vectCol.size()!=3 ){
-            trace.error() << "colors specification should contain R,G,B values (using default red)."<< std::endl;
-        }
-        lineColor.setRGBi(vectCol[0], vectCol[1], vectCol[2], 255);
+    lineColor.setRGBi(vectCol[0], vectCol[1], vectCol[2], 255);
+  }
+  if(vm.count("customPointColor"))
+  {
+    std::vector<unsigned int > vectCol = vm["customPointColor"].as<std::vector<unsigned int> >();
+    if(vectCol.size()!=3)
+    {
+      trace.error() << "colors specification should contain R,G,B values (using default red)."<< std::endl;
     }
-    if(vm.count("customPointColor")){
-        std::vector<unsigned int > vectCol = vm["customPointColor"].as<std::vector<unsigned int> >();
-        if(vectCol.size()!=3){
-            trace.error() << "colors specification should contain R,G,B values (using default red)."<< std::endl;
-        }
-        pointColor.setRGBi(vectCol[0], vectCol[1], vectCol[2], 255);
-    }
+    pointColor.setRGBi(vectCol[0], vectCol[1], vectCol[2], 255);
+  }
     
     
-    for(unsigned int i=0; i<vectPt1.size(); i++){
-        Z2i::Point pt1 (vectPt1[i][0], invertYaxis? height - vectPt1[i][1]: vectPt1[i][1] );
-        Z2i::Point pt2 (vectPt2[i][0], invertYaxis? height - vectPt2[i][1]: vectPt2[i][1] );
-        if(displayEndPoint)
-        {
-            aBoard.setPenColor(pointColor);
-            aBoard.fillCircle(pt1[0], pt1[1], 2*lineWidth);
-            aBoard.fillCircle(pt2[0], pt2[1], 2*lineWidth);
-        }
-        aBoard.setPenColor(lineColor);
-        aBoard.setLineWidth(lineWidth);
-        aBoard.drawLine(pt1[0], pt1[1], pt2[0], pt2[1]);
+  for(unsigned int i=0; i<vectPt1.size(); i++)
+  {
+    Z2i::Point pt1 (vectPt1[i][0], invertYaxis? height - vectPt1[i][1]: vectPt1[i][1] );
+    Z2i::Point pt2 (vectPt2[i][0], invertYaxis? height - vectPt2[i][1]: vectPt2[i][1] );
+    if(displayEndPoint)
+    {
+      aBoard.setPenColor(pointColor);
+      aBoard.fillCircle(pt1[0], pt1[1], 2*lineWidth);
+      aBoard.fillCircle(pt2[0], pt2[1], 2*lineWidth);
     }
+    aBoard.setPenColor(lineColor);
+    aBoard.setLineWidth(lineWidth);
+    aBoard.drawLine(pt1[0], pt1[1], pt2[0], pt2[1]);
+  }
     
 }
 
@@ -215,17 +217,17 @@ int main( int argc, char** argv )
   general_opt.add_options()
     ("help,h", "display this message")
     ("input,i", po::value<std::string>(), "the input file containing the segments x1 y1 x2 y2 to be displayed. ")
-    ("inputSecSet,s", po::value<std::string>(), "the second input set file containing the segments x1 y1 x2 y2 to be displayed. ")
+    ("inputSec,s", po::value<std::string>(), "the second input set file containing the segments x1 y1 x2 y2 to be displayed. ")
     ("SDPindex", po::value<std::vector <unsigned int> >()->multitoken(), "specify the sdp index of segment endpoints (by default 0,1,2,3).")
     ("domain", po::value<std::vector <int> >()->multitoken(), "limit the export to a given domain (xmin ymin xmax ymax).")
     ("lineWidth", po::value<double>()->default_value(1.0), "Define the linewidth of the segments.")
-    ("lineWidthSecSet", po::value<double>()->default_value(1.0), "Define the linewidth of the segments (second set).")
+    ("lineWidthSec", po::value<double>()->default_value(1.0), "Define the linewidth of the segments (second set).")
     ("noXFIGHeader", " to exclude xfig header in the resulting output stream (no effect with option -outputFile).")
     ("customLineColor",po::value<std::vector<unsigned int> >()->multitoken(), "set the R, G, B, A components of the colors of the mesh faces and eventually the color R, G, B, A of the mesh edge lines (set by default to black). " )
-    ("customLineColorSecSet",po::value<std::vector<unsigned int> >()->multitoken(), "set the R, G, B, A components of the colors of the mesh faces and eventually the color R, G, B, A of the mesh edge lines (set by default to black). " )
+    ("customLineColorSec",po::value<std::vector<unsigned int> >()->multitoken(), "set the R, G, B, A components of the colors of the mesh faces and eventually the color R, G, B, A of the mesh edge lines (set by default to black). " )
     ("customPointColor",po::value<std::vector<unsigned int> >()->multitoken(), "set the R, G, B, A components of the colors of the mesh faces and eventually the color R, G, B, A of the mesh edge lines (set by default to black). " )
-    ("noDisplayEndPoints,e", "to not displays segment end points.")
-    ("noDisplayEndPointsSecSet", "to not displays segment end points of second set.")
+    ("noDisplayEndPoints,e", "to not display segment end points.")
+    ("noDisplayEndPointsSec", "to not display segment end points of second set.")
     ("outputFile,o", po::value<std::string>(), " <filename> save output file automatically according the file format extension.")
     ("outputStreamEPS", " specify eps for output stream format.")
     ("outputStreamSVG", " specify svg for output stream format.")
@@ -239,127 +241,140 @@ int main( int argc, char** argv )
 
     ("backgroundImage", po::value<std::string>(), "backgroundImage <filename> : display image as background ")
     ("alphaBG", po::value<double>(), "alphaBG <value> 0-1.0 to display the background image in transparency (default 1.0), (transparency works only if cairo is available)")
-
     ("scale", po::value<double>(), "scale <value> 1: normal; >1 : larger ; <1 lower resolutions  )");
   
   
   bool parseOK=true;
   po::variables_map vm;
   try
-    {
-      po::store(po::parse_command_line(argc, argv, general_opt), vm);
-    }
+  {
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);
+  }
   catch(const std::exception& ex)
-    {
-      parseOK=false;
-      trace.info()<< "Error checking program options: "<< ex.what()<< endl;
-    }
+  {
+    parseOK=false;
+    trace.info()<< "Error checking program options: "<< ex.what()<< endl;
+  }
   
 
   // check if min arguments are given and tools description ------------------
   po::notify(vm);
   if( !parseOK || vm.count("help")||argc<=1)
+  {
+    std::cout << "Usage: " << argv[0] << " [input]\n"
+              << "Display line segments from input file where each segment is represented in one line."
+              << general_opt << "\n"
+              << "Typical use example:\n \t ./visualisation/displayLineSegments -i ../Samples/lines.sdp  --SDPindex 1 2 3 4  -o lines.eps --invertYaxis  \n";
+    return 0;
+  }
+  if(! vm.count("input"))
+  {
+    trace.error() << " The file name was not defined" << endl;
+    return 1;
+  }
+  
+  
+  double lineWidth =  vm["lineWidth"].as<double>();
+  double lineWidthSetB =  vm["lineWidthSec"].as<double>();
+  
+  double scale=1.0;
+  if(vm.count("scale"))
+  {
+    scale = vm["scale"].as<double>();
+  }
+  
+  Board2D aBoard;
+  aBoard.setUnit (0.05*scale, LibBoard::Board::UCentimeter);
+  if(vm.count("domain"))
+  {
+    std::vector<int> vectDom = vm["domain"].as<std::vector<int > >();
+    if(vectDom.size()!=4)
     {
-      std::cout << "Usage: " << argv[0] << " [input]\n"
-                << "Display line segments from input file where each segment is represented in one line."
-                << general_opt << "\n"
-                << "Typical use example:\n \t ./visualisation/displayLineSegments -i ../Samples/lines.sdp  --SDPindex 1 2 3 4  -o lines.eps --invertYaxis  \n";
+      trace.error() << "you need to specify the four values for the domain." << std::endl;
       return 0;
     }
-  if(! vm.count("input"))
-    {
-      trace.error() << " The file name was not defined" << endl;
-      return 1;
-    }
+    aBoard.setClippingRectangle(vectDom[0],vectDom[1],vectDom[2],vectDom[3]);
+  }
 
-
-   double lineWidth =  vm["lineWidth"].as<double>();
-   double lineWidthSetB =  vm["lineWidthSecSet"].as<double>();
-
-    double scale=1.0;
-   if(vm.count("scale")){
-     scale = vm["scale"].as<double>();
-   }
-  
-   Board2D aBoard;
-   aBoard.setUnit (0.05*scale, LibBoard::Board::UCentimeter);
-   if(vm.count("domain"))
-     {
-       std::vector<int> vectDom = vm["domain"].as<std::vector<int > >();
-       if(vectDom.size()!=4){
-         trace.error() << "you need to specify the four values for the domain." << std::endl;
-         return 0;
-       }
-       aBoard.setClippingRectangle(vectDom[0],vectDom[1],vectDom[2],vectDom[3]);
-     }
-
-   double alpha=1.0;
-   if(vm.count("alphaBG")){
-     alpha = vm["alphaBG"].as<double>();
-   }
-   unsigned int height = 0;
-   if(vm.count("backgroundImage")){
-     std::string imageName = vm["backgroundImage"].as<std::string>();
-     typedef ImageSelector<Z2i::Domain, unsigned char>::Type Image;
-     Image img = DGtal::GenericReader<Image>::import( imageName );
-     Z2i::Point ptInf = img.domain().lowerBound();
-     Z2i::Point ptSup = img.domain().upperBound();
-     unsigned int width = abs(ptSup[0]-ptInf[0]+1);
-     height = abs(ptSup[1]-ptInf[1]+1);
-     aBoard.drawImage(imageName, 0-0.5,height-0.5, width, height, -1, alpha );
-   }
+  double alpha=1.0;
+  if(vm.count("alphaBG"))
+  {
+    alpha = vm["alphaBG"].as<double>();
+  }
+  unsigned int height = 0;
+  if(vm.count("backgroundImage"))
+  {
+    std::string imageName = vm["backgroundImage"].as<std::string>();
+    typedef ImageSelector<Z2i::Domain, unsigned char>::Type Image;
+    Image img = DGtal::GenericReader<Image>::import( imageName );
+    Z2i::Point ptInf = img.domain().lowerBound();
+    Z2i::Point ptSup = img.domain().upperBound();
+    unsigned int width = abs(ptSup[0]-ptInf[0]+1);
+    height = abs(ptSup[1]-ptInf[1]+1);
+    aBoard.drawImage(imageName, 0-0.5,height-0.5, width, height, -1, alpha );
+  }
 
  
   bool invertYaxis = vm.count("invertYaxis");
 
-  if(vm.count("input")){
-      displayLineSet(vm, "input","customLineColor", aBoard, height, invertYaxis, !vm.count("noDisplayEndPoints"), lineWidth);
-   }
-   if(vm.count("inputSecSet")){
-      displayLineSet(vm, "inputSecSet","customLineColorSecSet", aBoard, height, invertYaxis,!vm.count("noDisplayEndPointsSecSte"), lineWidthSetB);
-   }
+  if(vm.count("input"))
+  {
+    displayLineSet(vm, "input","customLineColor", aBoard, height, invertYaxis, !vm.count("noDisplayEndPoints"), lineWidth);
+  }
+  if(vm.count("inputSec"))
+  {
+    displayLineSet(vm, "inputSec","customLineColorSec", aBoard, height, invertYaxis,!vm.count("noDisplayEndPointsSec"), lineWidthSetB);
+  }
 
  
   
-   if(vm.count("outputFile")){
-     std::string outputFileName= vm["outputFile"].as<std::string>();
-     std::string extension = outputFileName.substr(outputFileName.find_last_of(".") + 1);
-
-     if(extension=="svg"){
-       aBoard.saveSVG(outputFileName.c_str());
-     }
-     #ifdef WITH_CAIRO
-     else
-       if (extension=="eps"){
-         aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoEPS );
-       } else
-         if (extension=="pdf"){
-           aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoPDF );
-         } else
-           if (extension=="png"){
-             aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoPNG );
-           }
-     #endif
-     else if(extension=="eps"){
-       aBoard.saveEPS(outputFileName.c_str());
-     }else if(extension=="fig"){
-       aBoard.saveFIG(outputFileName.c_str(),LibBoard::Board::BoundingBox, 10.0, !vm.count("noXFIGHeader") );
-     }
-   }
+  if(vm.count("outputFile"))
+  {
+    std::string outputFileName= vm["outputFile"].as<std::string>();
+    std::string extension = outputFileName.substr(outputFileName.find_last_of(".") + 1);
+     
+    if(extension=="svg")
+    {
+      aBoard.saveSVG(outputFileName.c_str());
+    }
+#ifdef WITH_CAIRO
+    else
+      if (extension=="eps")
+      {
+        aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoEPS );
+      }
+      else if (extension=="pdf")
+      {
+        aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoPDF );
+      }
+      else if (extension=="png")
+      {
+        aBoard.saveCairo(outputFileName.c_str(),Board2D::CairoPNG );
+      }
+#endif
+      else if(extension=="eps")
+      {
+        aBoard.saveEPS(outputFileName.c_str());
+      }
+      else if(extension=="fig")
+      {
+        aBoard.saveFIG(outputFileName.c_str(),LibBoard::Board::BoundingBox, 10.0, !vm.count("noXFIGHeader") );
+      }
+  }
     
-     if (vm.count("outputStreamSVG")){
-     aBoard.saveSVG(std::cout);
-   } else
-       if (vm.count("outputStreamFIG")){
-     aBoard.saveFIG(std::cout, LibBoard::Board::BoundingBox, 10.0,  !vm.count("noXFIGHeader"));
-   } else
-         if (vm.count("outputStreamEPS")){
-     aBoard.saveEPS(std::cout);
-   }
+  if (vm.count("outputStreamSVG"))
+  {
+    aBoard.saveSVG(std::cout);
+  }
+  else if (vm.count("outputStreamFIG"))
+  {
+    aBoard.saveFIG(std::cout, LibBoard::Board::BoundingBox, 10.0,  !vm.count("noXFIGHeader"));
+  }
+  else if (vm.count("outputStreamEPS"))
+  {
+    aBoard.saveEPS(std::cout);
+  }
     
-
-  
-
   return 0;
 }
 
