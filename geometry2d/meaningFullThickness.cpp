5c178e70d0747e19897cc8f27327ddea89e49e2d
Kerautret <bertrand.kerautret@univ-lyon2.fr>
2020-09-29 11:26:51 +0200

    CLI11 in meaningFullThickness

--- geometry2d/meaningFullThickness.cpp
+++ geometry2d/meaningFullThickness.cpp
@@ -28,16 +28,15 @@
 #include <iostream>
 
 //boost
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/variables_map.hpp>
+#include "CLI11.hpp"
+
 
 //STL
 #include <vector>
 #include <string>
 #include <fstream>
 #include <iostream>
-
+#include <sstream>
 
 #include "DGtal/base/Common.h"
 #include "DGtal/helpers/StdDefs.h"
@@ -63,6 +62,49 @@
 #include "DGtal/math/MeaningfulScaleAnalysis.h"
 #include "DGtal/base/Circulator.h"
 #include "DGtal/math/MultiStatistics.h"
+
+/**
+ @page meaningFullThickness
+ 
+ @brief  Apply the Rosin Threshold algorithm.
+
+ @b Usage:   meaningFullThickness  [input]
+
+ @b Allowed @b options @b are :
+ 
+ @code
+ 
+ Positionals:
+   1 TEXT:FILE REQUIRED                  an input file.
+
+ Options:
+ Positionals:
+   1 TEXT:FILE REQUIRED                  the input contour.
+
+ Options:
+   -h,--help                             Print this help message and exit
+   -i,--input TEXT:FILE REQUIRED         the input contour.
+   -o,--outputDat TEXT=output.dat        the output dat file name.
+   -e,--outputEPS TEXT=output.dat        the output eps file name.
+   -f,--outputFIG TEXT=output.dat        the output fig file name.
+   -m,--scaleMax FLOAT=10                scale step to compute MT.
+   --drawingScale FLOAT=1                scale step to compute MT.
+   -s,--scaleStep FLOAT=1                scale step to compute MT.
+   -E,--EuclThickness                    use the euclidean thickness instead vertical/horizontal one.
+@endcode
+
+ @b Example:
+
+ @code
+     meaningFullThickness  -i ./Samples/ellipseBruit2.sdp   -e temp.eps  -o res.dat -E
+ @endcode
+
+ @image html resmeaningFullThickness .png "Example of result. "
+
+ @see
+ @ref meaningfullThickness.cpp
+
+ */
 
 
 using namespace DGtal;
@@ -144,60 +186,46 @@ getNoiseLevels(const std::vector<TInputPoint> &vectContour,
 
 
 
-
-
-///////////////////////////////////////////////////////////////////////////////
-namespace po = boost::program_options;
-
 int main( int argc, char** argv )
 {
+  // parse command line using CLI ----------------------------------------------
+  CLI::App app;
+  std::stringstream ssDescr;
+  ssDescr << "Display Meaningful Thickness " <<std::endl << "Basic usage: "<<std::endl
+  << "\t  " << argv[0] <<" -i ./Samples/ellipseBruit2.sdp   -e temp.eps  -o res.dat -E"<<std::endl;
+  app.description(ssDescr.str());
+  std::string inputFileName;
+  std::string outFileNameFIG {"output.fig"};
+  std::string outFileNameEPS {"output.eps"};
+  std::string outFileNamDAT {"output.dat"};
+  double scaleMax {10.0};
+  double scaleDrawing {1.0};
+  double scaleStep {1.0};
+  bool useEuclThickness {false};
+  app.add_option("-i,--input,1", inputFileName, "the input contour." )
+      ->required()
+  ->check(CLI::ExistingFile);
   
-  
-  // parse command line ----------------------------------------------
-  po::options_description general_opt("Allowed options are: ");
-  general_opt.add_options()
-  ("help,h", "display this message")
-  ("input,i", po::value<std::string>(), "the input contour. ")
-  ("outputDat,o", po::value<std::string>()->default_value("output.dat"), "the output dat file name. ")
-  ("outputEPS,e", po::value<std::string>()->default_value("output.eps"), "the output eps file name. ")
-  ("outputFIG,f", po::value<std::string>(), "the output fig file name. ")
-  ("scaleMax,m", po::value<double>()->default_value(10.0), "scale step to compute MT.")
-  ("drawingScale", po::value<double>()->default_value(1.0), "change the scale of out eps.")
-  ("scaleStep,s", po::value<double>()->default_value(1.0), "scale step to compute MT.")
-  ("EuclThickness,E", "use the euclidean thickness instead vertical/horizontal one.") ;
-  
-  
-  bool parseOK=true;
-  po::variables_map vm;
-  try{
-    po::store(po::parse_command_line(argc, argv, general_opt), vm);
-  }catch(const std::exception& ex){
-    parseOK=false;
-    trace.info()<< "Error checking program options: "<< ex.what()<< std::endl;
-  }
-  
-  po::notify(vm);
-  if(!parseOK||vm.count("help")||argc<=1 || (!(vm.count("input")) ))
-  {
-    trace.info()<< "Display Meaningful Thickness " <<std::endl << "Basic usage: "<<std::endl
-                << "\t  " << argv[0] <<" -i ./Samples/ellipseBruit2.sdp   -e temp.eps  -o res.dat -E"<<std::endl
-		  << general_opt << "\n";
-    return 0;
-  }
-  
-  double scaleMax = vm["scaleMax"].as<double>();
-  double scaleStep = vm["scaleStep"].as<double>();
-  double scaleDrawing = 1;
-  std::string fileName = vm["input"].as<std::string>();
-  functions::Hull2D::ThicknessDefinition thicknessDef = vm.count("EuclThickness") ? functions::Hull2D::EuclideanThickness:
+  auto optOutDAT = app.add_option("-o,--outputDat", outFileNamDAT, "the output dat file name.", true );
+  auto optOutEPS = app.add_option("-e,--outputEPS", outFileNamDAT, "the output eps file name.", true );
+  auto optOutFIG = app.add_option("-f,--outputFIG", outFileNamDAT, "the output fig file name.", true );
+  app.add_option("--scaleMax,-m", scaleMax, "scale step to compute MT.", true);
+  app.add_option("--drawingScale", scaleDrawing,  "scale step to compute MT.", true);
+  app.add_option("--scaleStep,-s",scaleStep, "scale step to compute MT.", true);
+  app.add_flag("--EuclThickness,-E", useEuclThickness, "use the euclidean thickness instead vertical/horizontal one.");
+ 
+  functions::Hull2D::ThicknessDefinition thicknessDef = useEuclThickness ? functions::Hull2D::EuclideanThickness:
   functions::Hull2D::HorizontalVerticalThickness;
   
+  app.get_formatter()->column_width(40);
+  CLI11_PARSE(app, argc, argv);
+  // END parse command line using CLI ----------------------------------------------
+
   
   Board2D aBoard;
   aBoard.setUnit (0.5*scaleDrawing, LibBoard::Board::UCentimeter);
   
-  std::vector<DGtal::Z2i::RealPoint> aContour = PointListReader<Z2i::RealPoint>::getPointsFromFile(fileName);
-  std::string outFileName = vm["outputEPS"].as<std::string>();
+  std::vector<DGtal::Z2i::RealPoint> aContour = PointListReader<Z2i::RealPoint>::getPointsFromFile(inputFileName);
   
   
   
@@ -225,27 +253,20 @@ int main( int argc, char** argv )
                     aContour[(i+1)%aContour.size()][0], aContour[(i+1)%aContour.size()][1]);
   }
   
-  aBoard.saveEPS(outFileName.c_str());
+  aBoard.saveEPS(outFileNameEPS.c_str());
   
-  if(vm.count("outputFIG")){
-    std::string outFileNameFIG = vm["outputFIG"].as<std::string>();
+  if(optOutFIG->count() >0){
     aBoard.saveFIG(outFileNameFIG.c_str());
   }
-
-  if(vm.count("outputDat")){
-    std::string outFileNameFIG = vm["outputDat"].as<std::string>();
+  
+  if(optOutDAT->count() > 0){
     std::ofstream f;
     f.open(outFileNameFIG.c_str());
     f<< "Meaningfull thickness generated from meaningfullThickness" << std::endl;
     for(unsigned int i =0; i< aContour.size(); i++){
       f << aContour[i][0] << " " <<  aContour[i][1] << " " << noiseLevel[i] << std::endl;
+    }
   }
-    
-  }
+  
 }
-
-
-
-
-
 
